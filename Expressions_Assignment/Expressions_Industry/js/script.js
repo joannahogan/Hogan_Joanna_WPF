1d48332c00d99e32bcd7da6e5b633e349165415e
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-11 19:13:29 -0400

    Calculating payrate

--- Expressions_Assignment/Expressions_Industry/js/script.js
+++ Expressions_Assignment/Expressions_Industry/js/script.js
@@ -3,6 +3,10 @@
 alert("Freelance Website Charge Calculator");
 console.log("Typical freelancers charge $40 to $75 per hour.");
 
+var experience = prompt("What is your experience level, 1-10?");
+var payRate = experience * 3.5 + 40;
+console.log("According to your experience, you should charge $" + payRate + " per hour.")
+
 
 
 
