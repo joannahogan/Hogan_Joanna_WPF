6a9bd185c6fa6231d2878ed14e971da402ce9032
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-11 16:09:15 -0400

    Comments

--- Expressions_Assignment/Expressions_Personal/js/script.js
+++ Expressions_Assignment/Expressions_Personal/js/script.js
@@ -1,25 +1,25 @@
 /*** Joanna Hogan 9/11/14 Personal ***/
 
-var zombies = "The Walking Dead";
-var seasons = [6, 13, 16, 16];
+var zombies = "The Walking Dead"; //Defined string
+var seasons = [6, 13, 16, 16]; //Episodes per season in Array
 
-var totalEpisodes = seasons[0] + seasons[1] + seasons[2] + seasons[3];
+var totalEpisodes = seasons[0] + seasons[1] + seasons[2] + seasons[3]; //Total of episodes per season
 
-console.log("There have been " + totalEpisodes + " total episodes of " + zombies + " in 4 seasons.");
+console.log("There have been " + totalEpisodes + " total episodes of " + zombies + " in 4 seasons."); //Print of total episodes
 
-var watched = prompt(zombies + " returns to AMC on October 12, 2014. \n\nHow many episodes have you watched to completion?");
-var percentage = watched/51 * 100;
+var watched = prompt(zombies + " returns to AMC on October 12, 2014. \n\nHow many episodes have you watched to completion?"); //User input how many episodes watched
+var percentage = watched/51 * 100; //Percentage of episodes watched
 
-console.log("You have watched " + watched + " episodes of " + zombies + ". That's " + percentage + "%.");
+console.log("You have watched " + watched + " episodes of " + zombies + ". That's " + percentage + "%."); //Print of percentage
 
-var episodesLeft = totalEpisodes - watched;
+var episodesLeft = totalEpisodes - watched; //Calculated how many episodes left
 
-var daysLeft = prompt("How many days do you have left to catch up before the season premier?");
+var daysLeft = prompt("How many days do you have left to catch up before the season premier?"); //User input how many days
 
-var dailyEpisodes = daysLeft/episodesLeft;
-var weeklyEpisodes = dailyEpisodes * 7;
+var dailyEpisodes = daysLeft/episodesLeft; //Calculates episodes per day
+var weeklyEpisodes = dailyEpisodes * 7;  //Calculates episodes per week
 
-console.log("To finish the remaining " + episodesLeft + " episodes, you would have to watch " + weeklyEpisodes + " per week or " + dailyEpisodes + " per day.");
+console.log("To finish the remaining " + episodesLeft + " episodes, you would have to watch " + weeklyEpisodes + " per week or " + dailyEpisodes + " per day."); //Prints out how many episodes must be watched by day and week
 
 
 
