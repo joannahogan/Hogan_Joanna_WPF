dca02547f413a3df9faf6214c3fbc0d6fbb3f518
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-11 15:01:55 -0400

    Wacky Alerts

--- Expressions_Assignment/Expressions_Wacky/js/script.js
+++ Expressions_Assignment/Expressions_Wacky/js/script.js
@@ -1,2 +1,6 @@
 /*** Joanna Hogan 9/11/14 Wacky ***/
 
+alert("Prepare to be amazed.\n\n\nNo, really prepare. I'll wait.\n\nYou will need the following:\n- Paper and Pen\n- Your \'Beginning Javascript\' book");
+
+alert("Using legitimate magic, I shall attempt to predict the future.");
+
