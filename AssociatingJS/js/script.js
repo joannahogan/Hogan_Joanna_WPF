57aaf0dfe3b890fc2515fc26e3d2a60bc390d4d7
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-11 09:54:52 -0400

    Expressions with Arrays

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,9 +1,6 @@
-//Expressions - PEMDAS - Order of Operations
+//Expressions with Arrays
 
+var orangeBins = [234, 567, 883];
 
-var quiz1 = 87;
-var quiz2 = 100;
-var quiz3 = 60;
-var quiz4 = 80;
-
-var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
