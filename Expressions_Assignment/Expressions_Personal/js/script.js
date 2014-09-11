0a394d7e2a35cad3b91a326780a38e1ff622be84
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-11 16:04:04 -0400

    Prompt episodes watched
    returned percentage

--- Expressions_Assignment/Expressions_Personal/js/script.js
+++ Expressions_Assignment/Expressions_Personal/js/script.js
@@ -5,6 +5,13 @@ var seasons = [6, 13, 16, 16];
 
 var totalEpisodes = seasons[0] + seasons[1] + seasons[2] + seasons[3];
 
+console.log("There have been " + totalEpisodes + " total episodes of " + zombies + " in 4 seasons.");
+
+var watched = prompt(zombies + " returns to AMC on October 12, 2014. \n\nHow many episodes have you watched to completion?");
+var percentage = watched/51 * 100;
+
+console.log("You have watched " + watched + " episodes of " + zombies + ". That's " + percentage + "%.");
+
 
 
 
