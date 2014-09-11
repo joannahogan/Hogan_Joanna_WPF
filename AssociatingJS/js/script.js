27c92bfa5897efbe4c35f2196a917c6ace079c41
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-11 09:34:08 -0400

    Expressions

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,21 +1,6 @@
 alert("This is in the js file.");
 
-var yearBorn = 1955;
-console.log(yearBorn);
-
-var age; //declaration
-age = 32; //definition
-
-var yearOfBirth = 1970;
-
-var isStudent = false; //Boolean Variable
-
-var firstName = "Joanna";
-
-var phrase = 'I don\'t know \n You\'ll have to ask'; //Escaping character
-
-// Arrays
-
-var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"];
-var num = 1;
-console.log(muppetNames[num]);
+//Expressions
+var a = 2;
+a = a + 3;
+console.log(a);
