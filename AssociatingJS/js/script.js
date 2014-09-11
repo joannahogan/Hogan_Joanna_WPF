53115aa3106ed6c64f6b35c74d27b8b09bac0f30
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-11 09:51:50 -0400

    Order of Operations

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,7 +1,9 @@
-//Expressions - Arithmetic Operators
+//Expressions - PEMDAS - Order of Operations
 
-//area of triangle is half width x height
-var width = 4;
-var height = 5;
-var area = width * height/2;
-console.log(area);
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
