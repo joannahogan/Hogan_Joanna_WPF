cf0ac8d29fbec5c6c364321aa510accb9e0aff65
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-11 15:02:49 -0400

    Prompting for numbers 1 and 2

--- Expressions_Assignment/Expressions_Wacky/js/script.js
+++ Expressions_Assignment/Expressions_Wacky/js/script.js
@@ -4,3 +4,9 @@ alert("Prepare to be amazed.\n\n\nNo, really prepare. I'll wait.\n\nYou will nee
 
 alert("Using legitimate magic, I shall attempt to predict the future.");
 
+var number1 = prompt("First, pick any 3-digit number comprised of 3 different numbers. \n\nEx. \"942\"");
+console.log(number1);
+
+var number2 = prompt("Reverse the number and subtract the smaller number from the larger. \n\nEnter the reversed number here and I will do the math for you:");
+console.log(number2);
+
