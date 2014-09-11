720fe848d82912975077665224d8e06ee392010a
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-11 10:16:56 -0400

    Assignment Operators

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,10 +1,9 @@
-//Casting
+//Assignment Operators
 
-var areaCode = 407;
-var firstPart = 203;
-var secPart = 4529;
+var a = 3;
+a += 4; //Adds and changes a
+console.log(a);
 
-var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart);
-console.log(phoneNo);
+//a ++; //a = a + 1 //a += 1 //Only works with 1
 
 
