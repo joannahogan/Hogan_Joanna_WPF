c91719f2264ecf108c78787b733db4177906e5a2
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-11 19:18:35 -0400

    Comments

--- Expressions_Assignment/Expressions_Industry/js/script.js
+++ Expressions_Assignment/Expressions_Industry/js/script.js
@@ -1,14 +1,14 @@
 /*** Joanna Hogan 9/11/14 Industry Specific ***/
 
-alert("Freelance Website Charge Calculator");
-console.log("Typical freelancers charge $40 to $75 per hour.");
+alert("Freelance Website Charge Calculator"); //Alert explaining calculator
+console.log("Typical freelancers charge $40 to $75 per hour."); //Average pay rates for freelancers for user reference
 
-var experience = prompt("What is your experience level, 1-10?");
-var payRate = experience * 3.5 + 40;
-console.log("According to your experience, you should charge $" + payRate + " per hour.")
+var experience = prompt("What is your experience level, 1-10?"); //User enter experience level
+var payRate = experience * 3.5 + 40; //Pay rate calculated by experience
+console.log("According to your experience, you should charge $" + payRate + " per hour.") //Print out of pay rate
 
-var hoursWorked = prompt("How many total hours did you put into the project?");
-console.log(hoursWorked + " total hours");
+var hoursWorked = prompt("How many total hours did you put into the project?"); //User input hours worked
+console.log(hoursWorked + " total hours"); //Print hours to console
 
 var learningCurve = prompt("How many of those hours were spent learning new software or fixing mistakes?");
 
