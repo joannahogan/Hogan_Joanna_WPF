49cb38c35617189e9f2a01c9e5444d7cb0177a47
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-11 19:13:03 -0400

    Freelance calculator

--- Expressions_Assignment/Expressions_Industry/js/script.js
+++ Expressions_Assignment/Expressions_Industry/js/script.js
@@ -1,1 +1,15 @@
 /*** Joanna Hogan 9/11/14 Industry Specific ***/
+
+alert("Freelance Website Charge Calculator");
+console.log("Typical freelancers charge $40 to $75 per hour.");
+
+
+
+
+
+
+
+
+
+
+
