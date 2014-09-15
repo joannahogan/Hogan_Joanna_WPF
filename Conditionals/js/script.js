6912e05764d02e5e0a0af1efa1741c40de677f13
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-15 15:02:52 -0400

    Relational Expression
    Adding variables

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -3,9 +3,10 @@
 
 //Relational Expression
 
-var kidHeight = 50;
+var kidHeight = 47;
 var minHeight = 48;
+var sneakerLift = 2;
 
-if(kidHeight > minHeight){
+if(kidHeight + sneakerLift > minHeight){
     console.log("You can ride the coaster!");
 }
