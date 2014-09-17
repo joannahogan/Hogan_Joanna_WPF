187a4fe642a71d5fb9315f7b5a56b8bd8486477f
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-17 19:22:12 -0400

    Celsius to Fahrenheit
    100 to 212

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -4,15 +4,17 @@
 //Expressions with Conditionals
 
 
-var unit = 32;
+//var unit = 32;
+var unit = 100;
 
 var userInput = prompt("Type C for Celsius, or F for Fahrenheit.");
 
 if (userInput === "c"){
-    var unit = (unit - 32) * 5/9;
+    //var unit = (unit - 32) * 5/9;
     var degrees = "Celsius";
 }else if(userInput === "f"){
     var degrees = "Fahrenheit";
+    var unit = unit * 9/5 + 32;
 }
 console.log("The temperature is " + unit + "° " + degrees);
 
