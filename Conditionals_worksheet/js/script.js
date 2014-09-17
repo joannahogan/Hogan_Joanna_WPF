be14c83f0d421d30da2032c72df7d1fa066103b8
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-17 19:24:06 -0400

    Fahrenheit to Celsius
    90 to 32.222

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -5,16 +5,17 @@
 
 
 //var unit = 32;
-var unit = 100;
+//var unit = 100;
+var unit = 90;
 
 var userInput = prompt("Type C for Celsius, or F for Fahrenheit.");
 
 if (userInput === "c"){
-    //var unit = (unit - 32) * 5/9;
+    var unit = (unit - 32) * 5/9;
     var degrees = "Celsius";
 }else if(userInput === "f"){
     var degrees = "Fahrenheit";
-    var unit = unit * 9/5 + 32;
+    //var unit = unit * 9/5 + 32;
 }
 console.log("The temperature is " + unit + "° " + degrees);
 
