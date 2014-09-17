47a35ff5010f6de5f97232a6ccef6ae77f41d951
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-17 16:07:29 -0400

    Else If

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -5,9 +5,12 @@
 
 var kidHeight = 52;
 var minHeight = 48;
+var withParHeight = 45;
 
 if(kidHeight > minHeight){
     console.log("You can ride the coaster!");
+}else if(kidHeight > withParHeight){
+    console.log("You can ride, but only with a parent.");
 }else{
     console.log("Sorry kid, you've got some growing to do!");
 }
