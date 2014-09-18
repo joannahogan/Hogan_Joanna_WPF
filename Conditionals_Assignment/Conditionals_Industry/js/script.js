94970c7bac1f713382a286816abe21aae6f0c0ae
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-17 20:58:06 -0400

    Added sponsor variable

--- Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -3,13 +3,16 @@
 
 var age = 29;
 var member = false;
-
+var withSponsor = true;
 
 (age >= 21) ? console.log("You are old enough to enter the private club.") : console.log("You are not old enough for this scene.")
 
 if (age >= 21 && member === true) {
     console.log("Go on in. Thanks for your membership.");
+}else if (age >= 21 && withSponsor === true){
+    console.log("Have a good time with your sponsor. Please consider becoming a member.")
 }
+
 
 
 
