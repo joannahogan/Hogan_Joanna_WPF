93b04b51656b678761d1f4009bd526c1303d4da0
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-18 15:23:50 -0400

    Entered player two conditional statements

--- Conditionals_Assignment/Conditionals_Wacky/js/script.js
+++ Conditionals_Assignment/Conditionals_Wacky/js/script.js
@@ -5,8 +5,8 @@
 var playerOne = prompt("Player 1: Do you choose Rock, Paper, Scissors, Lizard, or Spock?");//User prompt for input
 var playerTwo = prompt("Player 2: Do you choose Rock, Paper, Scissors, Lizard, or Spock?");//User prompt for input
 
-if (playerOne === "rock" && playerTwo === "scissors"){  //if statement, when rock and scissors chosen by users
-    console.log("Rock crushes Scissors!"); //print response
+if (playerOne === "rock" && playerTwo === "scissors"){
+    console.log("Rock crushes Scissors!");
 }else if (playerOne === "scissors" && playerTwo === "paper") {
     console.log("Scissors cust Paper!");
 }else if (playerOne === "paper" && playerTwo === "rock") {
@@ -24,6 +24,26 @@ if (playerOne === "rock" && playerTwo === "scissors"){  //if statement, when roc
 }else if (playerOne === "paper" && playerTwo === "spock") {
     console.log("Paper disproves Spock!");
 }else if (playerOne === "spock" && playerTwo === "rock") {
+    console.log("Spock vaporizes Rock!");
+}ELSE if (playerTwo === "rock" && playerOne === "scissors"){
+    console.log("Rock crushes Scissors!");
+}else if (playerTwo === "scissors" && playerOne === "paper") {
+    console.log("Scissors cust Paper!");
+}else if (playerTwo === "paper" && playerOne === "rock") {
+    console.log("Paper covers Rock!");
+}else if (playerTwo === "rock" && playerOne === "lizard") {
+    console.log("Rock crushes Lizard!");
+}else if (playerTwo === "lizard" && playerOne === "spock") {
+    console.log("Lizard poisons Spock!");
+}else if (playerTwo === "spock" && playerOne === "scissors") {
+    console.log("Spock smashes Scissors!");
+}else if (playerTwo === "scissors" && playerOne === "lizard") {
+    console.log("Scissors decapitates Lizard!");
+}else if (playerTwo === "lizard" && playerOne === "paper") {
+    console.log("Lizard eats Paper!");
+}else if (playerTwo === "paper" && playerOne === "spock") {
+    console.log("Paper disproves Spock!");
+}else if (playerTwo === "spock" && playerOne === "rock") {
     console.log("Spock vaporizes Rock!");
 }
 
