fcb5c463911fe25d2dc93a41ca5462f1e1cc25e6
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-17 21:12:44 -0400

    Added prompts for user info

--- Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -3,15 +3,16 @@
 
 //Private club example that requires age and membership
 
-var age = 29;  //Age of person trying to enter
-var member = false; //Are they a member?
-var withSponsor = true;  //Are they with a sponsor?
+var age = prompt("Enter your age."); //Prompt user for age
+var member = prompt("Are you a member of this club?");//Prompt user for membership
+var withSponsor = prompt("Are you with a sponsor");//prompt user for sponsorship
 
 (age >= 21) ? console.log("You are old enough to enter the private club.") : console.log("You are not old enough for this scene."); //Ternary statement
 
-if (age >= 21 && member === true) { //If statement with relational operator
+
+if (age >= 21 && member === "yes") { //If statement with relational operator
     console.log("Go on in. Thanks for your membership."); //Console print if conditions met
-}else if (age >= 21 && withSponsor === true){ //Else if statement with relational operator
+}else if (age >= 21 && withSponsor === "yes"){ //Else if statement with relational operator
     console.log("Have a good time with your sponsor. Please consider becoming a member."); //Console print if conditions met
 }
 
