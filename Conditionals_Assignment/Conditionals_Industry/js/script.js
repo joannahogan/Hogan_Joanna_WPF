6039ed0d7443f314dd33cf07eb934429cb2202c4
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-17 21:01:11 -0400

    Missing semicolons
    and added comments

--- Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -3,16 +3,16 @@
 
 //Private club example that requires age and membership
 
-var age = 29;
-var member = false;
-var withSponsor = true;
+var age = 29;  //Age of person trying to enter
+var member = false; //Are they a member?
+var withSponsor = true;  //Are they with a sponsor?
 
-(age >= 21) ? console.log("You are old enough to enter the private club.") : console.log("You are not old enough for this scene.")
+(age >= 21) ? console.log("You are old enough to enter the private club.") : console.log("You are not old enough for this scene.");
 
 if (age >= 21 && member === true) {
     console.log("Go on in. Thanks for your membership.");
 }else if (age >= 21 && withSponsor === true){
-    console.log("Have a good time with your sponsor. Please consider becoming a member.")
+    console.log("Have a good time with your sponsor. Please consider becoming a member.");
 }
 
 
