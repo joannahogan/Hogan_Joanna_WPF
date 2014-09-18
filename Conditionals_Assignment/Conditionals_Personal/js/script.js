ab07dc8b11fcbf4daa4b606d32a183ed40690048
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-18 16:04:08 -0400

    Check for user input

--- Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -4,10 +4,14 @@
 // If I have don't have Starbucks or I skip lunch, I can go to the movies
 
 var starbucks = prompt("Did you go to Starbucks this morning?");  //user input if bought starbucks
-var skiplunch = prompt("Did you skip lunch today?");  //user input if bought lunch
+var skipLunch = prompt("Did you skip lunch today?");  //user input if bought lunch
+
+if (starbucks === "" || skipLunch === ""){  //if user forgets input
+    console.log("You did not input your answer.");  //console response for no input
+}
 
 
-if (starbucks === "no" || skiplunch === "yes") {  //if user skipped lunch or starbucks conditional
+if (starbucks === "no" || skipLunch === "yes") {  //if user skipped lunch or starbucks conditional
     console.log("You can go to the movies!");  //console print response
 } else {
     console.log("Sorry, you don't have enough money for the movies today.");  //console response for other inputs
