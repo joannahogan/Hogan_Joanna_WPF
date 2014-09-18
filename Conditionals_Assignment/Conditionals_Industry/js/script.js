217fd2f1d8c169e3cfb68d6e6b528b6ba40a5065
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-17 20:57:45 -0400

    Added member variable

--- Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -1,13 +1,15 @@
 /*** Joanna Hogan  9/17/14  Conditionals Industry ***/
 
 
-var age = 20;
+var age = 29;
+var member = false;
 
 
-(age >= 21) ? console.log("You are old enough to enter.") : console.log("You are not old enough for this scene.")
+(age >= 21) ? console.log("You are old enough to enter the private club.") : console.log("You are not old enough for this scene.")
 
-
-
+if (age >= 21 && member === true) {
+    console.log("Go on in. Thanks for your membership.");
+}
 
 
 
