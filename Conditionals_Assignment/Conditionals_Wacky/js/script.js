07c348dc5a692c68e5a86f862ff50a196c5f456a
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-18 15:35:13 -0400

    Comments added

--- Conditionals_Assignment/Conditionals_Wacky/js/script.js
+++ Conditionals_Assignment/Conditionals_Wacky/js/script.js
@@ -5,46 +5,46 @@
 var playerOne = prompt("Player 1: Do you choose rock, paper, scissors, lizard, or spock?");//User prompt for input
 var playerTwo = prompt("Player 2: Do you choose rock, paper, scissors, lizard, or spock??");//User prompt for input
 
-if (playerOne === "rock" && playerTwo === "scissors"){
-    console.log("Rock crushes Scissors!");
-}else if (playerOne === "scissors" && playerTwo === "paper") {
-    console.log("Scissors cust Paper!");
-}else if (playerOne === "paper" && playerTwo === "rock") {
-    console.log("Paper covers Rock!");
-}else if (playerOne === "rock" && playerTwo === "lizard") {
-    console.log("Rock crushes Lizard!");
-}else if (playerOne === "lizard" && playerTwo === "spock") {
-    console.log("Lizard poisons Spock!");
-}else if (playerOne === "spock" && playerTwo === "scissors") {
-    console.log("Spock smashes Scissors!");
-}else if (playerOne === "scissors" && playerTwo === "lizard") {
-    console.log("Scissors decapitates Lizard!");
-}else if (playerOne === "lizard" && playerTwo === "paper") {
-    console.log("Lizard eats Paper!");
-}else if (playerOne === "paper" && playerTwo === "spock") {
-    console.log("Paper disproves Spock!");
-}else if (playerOne === "spock" && playerTwo === "rock") {
-    console.log("Spock vaporizes Rock!");
-}else if (playerTwo === "rock" && playerOne === "scissors"){
-    console.log("Rock crushes Scissors!");
-}else if (playerTwo === "scissors" && playerOne === "paper") {
-    console.log("Scissors cust Paper!");
-}else if (playerTwo === "paper" && playerOne === "rock") {
-    console.log("Paper covers Rock!");
-}else if (playerTwo === "rock" && playerOne === "lizard") {
-    console.log("Rock crushes Lizard!");
-}else if (playerTwo === "lizard" && playerOne === "spock") {
-    console.log("Lizard poisons Spock!");
-}else if (playerTwo === "spock" && playerOne === "scissors") {
-    console.log("Spock smashes Scissors!");
-}else if (playerTwo === "scissors" && playerOne === "lizard") {
-    console.log("Scissors decapitates Lizard!");
-}else if (playerTwo === "lizard" && playerOne === "paper") {
-    console.log("Lizard eats Paper!");
-}else if (playerTwo === "paper" && playerOne === "spock") {
-    console.log("Paper disproves Spock!");
-}else if (playerTwo === "spock" && playerOne === "rock") {
-    console.log("Spock vaporizes Rock!");
+if (playerOne === "rock" && playerTwo === "scissors"){ //User inputs rock and scissors
+    console.log("Rock crushes Scissors!");  //Print response for rock and scissors
+}else if (playerOne === "scissors" && playerTwo === "paper") {  //User inputs for scissors and paper
+    console.log("Scissors cust Paper!");  //Print response for scissors and paper
+}else if (playerOne === "paper" && playerTwo === "rock") { //User inputs for paper and rock
+    console.log("Paper covers Rock!");  //print response for paper and rock
+}else if (playerOne === "rock" && playerTwo === "lizard") {  //user inputs rock and lizard
+    console.log("Rock crushes Lizard!");  //print response for rock and lizard
+}else if (playerOne === "lizard" && playerTwo === "spock") { //user inputs for lizard and spock
+    console.log("Lizard poisons Spock!");  //print response for lizard and spock
+}else if (playerOne === "spock" && playerTwo === "scissors") {  //user input for spock and scissors
+    console.log("Spock smashes Scissors!");  //print response for spock and scissors
+}else if (playerOne === "scissors" && playerTwo === "lizard") {  //user inputs for scissors and lizard
+    console.log("Scissors decapitates Lizard!");  //print response for scissors and lizard
+}else if (playerOne === "lizard" && playerTwo === "paper") {  //user inputs lizard and paper
+    console.log("Lizard eats Paper!");  //print response for lizard and paper
+}else if (playerOne === "paper" && playerTwo === "spock") {  //user inputs paper and spock
+    console.log("Paper disproves Spock!");  //print response paper and spock
+}else if (playerOne === "spock" && playerTwo === "rock") {  //user inputs spock and rock
+    console.log("Spock vaporizes Rock!");  //print response spock and rock
+}else if (playerTwo === "rock" && playerOne === "scissors"){  //User inputs rock and scissors
+    console.log("Rock crushes Scissors!");  //Print response for rock and scissors
+}else if (playerTwo === "scissors" && playerOne === "paper") {  //User inputs for scissors and paper
+    console.log("Scissors cust Paper!");  //Print response for scissors and paper
+}else if (playerTwo === "paper" && playerOne === "rock") {  //User inputs for paper and rock
+    console.log("Paper covers Rock!");  //print response for paper and rock
+}else if (playerTwo === "rock" && playerOne === "lizard") {  //user inputs rock and lizard
+    console.log("Rock crushes Lizard!");  //print response for rock and lizard
+}else if (playerTwo === "lizard" && playerOne === "spock") {  //user inputs for lizard and spock
+    console.log("Lizard poisons Spock!");  //print response for lizard and spock
+}else if (playerTwo === "spock" && playerOne === "scissors") {  //user input for spock and scissors
+    console.log("Spock smashes Scissors!");  //print response for spock and scissors
+}else if (playerTwo === "scissors" && playerOne === "lizard") {  //user inputs for scissors and lizard
+    console.log("Scissors decapitates Lizard!");  //print response for scissors and lizard
+}else if (playerTwo === "lizard" && playerOne === "paper") {  //user inputs lizard and paper
+    console.log("Lizard eats Paper!");  //print response for lizard and paper
+}else if (playerTwo === "paper" && playerOne === "spock") {  //user inputs paper and spock
+    console.log("Paper disproves Spock!");  //print response paper and spock
+}else if (playerTwo === "spock" && playerOne === "rock") {  //user inputs spock and rock
+    console.log("Spock vaporizes Rock!");  //print response spock and rock
 }
 
 
