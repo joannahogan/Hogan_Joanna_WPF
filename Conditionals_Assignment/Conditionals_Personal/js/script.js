95126a02bac93c75dfa99b5499a2348c7af14e65
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-18 16:00:15 -0400

    Comments

--- Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -3,14 +3,14 @@
 
 // If I have don't have Starbucks or I skip lunch, I can go to the movies
 
-var starbucks = prompt("Did you go to Starbucks this morning?");
-var skiplunch = prompt("Did you skip lunch today?");
+var starbucks = prompt("Did you go to Starbucks this morning?");  //user input if bought starbucks
+var skiplunch = prompt("Did you skip lunch today?");  //user input if bought lunch
 
 
-if (starbucks === "no" || skiplunch === "yes") {
-    console.log("You can go to the movies!");
+if (starbucks === "no" || skiplunch === "yes") {  //if user skipped lunch or starbucks conditional
+    console.log("You can go to the movies!");  //console print response
 } else {
-    console.log("Sorry, you don't have enough money for the movies today.");
+    console.log("Sorry, you don't have enough money for the movies today.");  //console response for other inputs
 }
 
 
