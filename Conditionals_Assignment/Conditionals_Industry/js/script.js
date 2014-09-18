8c0ca9f271592dad6bb0a5087beb8bf396406ffb
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-17 21:02:47 -0400

    Comments

--- Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -7,12 +7,12 @@ var age = 29;  //Age of person trying to enter
 var member = false; //Are they a member?
 var withSponsor = true;  //Are they with a sponsor?
 
-(age >= 21) ? console.log("You are old enough to enter the private club.") : console.log("You are not old enough for this scene.");
+(age >= 21) ? console.log("You are old enough to enter the private club.") : console.log("You are not old enough for this scene."); //Ternary statement
 
-if (age >= 21 && member === true) {
-    console.log("Go on in. Thanks for your membership.");
-}else if (age >= 21 && withSponsor === true){
-    console.log("Have a good time with your sponsor. Please consider becoming a member.");
+if (age >= 21 && member === true) { //If statement with relational operator
+    console.log("Go on in. Thanks for your membership."); //Console print if conditions met
+}else if (age >= 21 && withSponsor === true){ //Else if statement with relational operator
+    console.log("Have a good time with your sponsor. Please consider becoming a member."); //Console print if conditions met
 }
 
 
