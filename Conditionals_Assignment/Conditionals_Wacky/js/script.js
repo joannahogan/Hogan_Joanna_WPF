31f3d16be705cd6db12d29e867c8b66a5146b67e
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-18 15:07:05 -0400

    User input Rock

--- Conditionals_Assignment/Conditionals_Wacky/js/script.js
+++ Conditionals_Assignment/Conditionals_Wacky/js/script.js
@@ -2,7 +2,12 @@
 
 
 
-var userInput = prompt("Do you choose Rock, Paper, Scissors, Lizard, or Spock?");
+var userInput = prompt("Do you choose Rock, Paper, Scissors, Lizard, or Spock?");//User prompt for input
+
+if (userInput === "rock"){  //if statement, when rock chosen by user
+    console.log("You can crush Scissors and Lizards with your Rock, but be covered by Paper or vaporized by Spock."); //print response for rock
+
+
 
 
 
