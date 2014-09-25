9980da7e5534fa5e2369a8d034e9cf7920c75d3a
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-25 18:54:38 -0400

    Variables and Functions

--- Functions_Assignment/Functions_Personal/js/script.js
+++ Functions_Assignment/Functions_Personal/js/script.js
@@ -1,9 +1,24 @@
 /*** Joanna Hogan 9/25/14 Personal ***/
 
 
+var twelveOz = 44;
+var twentyOz = 65;
+var weeks = 52;
 
 
+var howMany = prompt("How many sodas do you consume per week?");
+var sodaSize = prompt("Do you typically drink soda from a can or bottle?");
 
+
+function calcCan(x,y) {
+    var amtSugar = x * y * weeks;
+    console.log("You consume " + amtSugar + " grams of sugar per year.");
+}
+
+function calcBottle(x,y) {
+    var amtSugar = x * y * weeks;
+    console.log("You consume " + amtSugar + " grams of sugar per year.");
+}
 
 
 
