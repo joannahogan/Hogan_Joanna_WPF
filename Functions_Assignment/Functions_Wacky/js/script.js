2bd5de2657d84d8893132cf71bf6c2581252898b
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-25 21:28:31 -0400

    Comments

--- Functions_Assignment/Functions_Wacky/js/script.js
+++ Functions_Assignment/Functions_Wacky/js/script.js
@@ -1,25 +1,27 @@
 /*** Joanna Hogan 9/25/14 Wacky ***/
 
 
-var guests = prompt("Wedding Cake Calculator\n\nHow many guests will be at your wedding?");
-var servings = prompt("Do you want to let your guests have a second helping?");
+var guests = prompt("Wedding Cake Calculator\n\nHow many guests will be at your wedding?");//user prompt for number
+var servings = prompt("Do you want to let your guests have a second helping?");//user prompt for string
 
-var servingSize = 8;
+var servingSize = 8; //variable serving size
 
-function cake(g,s){
-    var totalCake = g * s;
-    return totalCake;
+function cake(g,s){ //function cake
+    var totalCake = g * s; //calculate total cake
+    return totalCake; //return value
 }
 
-var howMuchCake = cake(guests, servingSize);
+var howMuchCake = cake(guests, servingSize);  //call function
 
-if (servings == "yes"){
+if (servings == "yes"){ 
     var total = howMuchCake * 2;
     console.log("You will need " + total + " square inches of cake.");
 } else {
     var total = howMuchCake;
     console.log("You will need " + total + " square inches of cake.");
 }
+
+
 
 
 
