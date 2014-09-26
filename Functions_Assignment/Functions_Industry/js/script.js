6b189e4724f1435f8ba445428c7f39fd3fa3cfb6
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-09-25 20:26:08 -0400

    Comments

--- Functions_Assignment/Functions_Industry/js/script.js
+++ Functions_Assignment/Functions_Industry/js/script.js
@@ -2,21 +2,21 @@
 
 
 
-alert("The Area of a Trapezoid is the average of the two base lengths times the altitude.");
-var baseOne = prompt("How many meters is the length of base 1?");
-var baseTwo = prompt("How many meters is the length of base 2?");
-var alt = prompt("What is the altitude of the trapezoid?");
+alert("The Area of a Trapezoid is the average of the two base lengths times the altitude."); //purpose of calculations
+var baseOne = prompt("How many meters is the length of base 1?"); //prompt for base
+var baseTwo = prompt("How many meters is the length of base 2?"); //prompt for base
+var alt = prompt("What is the altitude of the trapezoid?"); //prompt for height
 
 
 
-var trapArea = function (a,b,h) {
-    var area = (a+b)/2 * h;
-    return area;
+var trapArea = function (a,b,h) { //Anonymous function
+    var area = (a+b)/2 * h; //calculate area
+    return area;  //return value
 }
 
-var total = trapArea(baseOne,baseTwo,alt);
+var total = trapArea(baseOne,baseTwo,alt);  //call function, store in total
 
-console.log("The area of your trapezoid is " + total + " square meters.");
+console.log("The area of your trapezoid is " + total + " square meters."); //print to console
 
 
 
